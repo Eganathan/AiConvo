01fa1d9334e6ba986eedf6227bd8c31860e9ae2e
eganathan.r <eganathan.r@zohocorp.com>
2023-12-15 23:21:24 +0530

    chore: new icon and extracted the cht bubble

--- app/src/main/java/dev/eknath/aiconvo/MainActivity.kt
+++ app/src/main/java/dev/eknath/aiconvo/MainActivity.kt
@@ -1,18 +1,9 @@
 package dev.eknath.aiconvo
 
 import Loading
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.Animatable
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,36 +14,24 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -60,15 +39,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -76,26 +52,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.lerp
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Fill
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultBlendMode
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import androidx.core.os.BuildCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.ai.client.generativeai.GenerativeModel
-import com.google.ai.client.generativeai.type.GenerationConfig
-import com.google.ai.client.generativeai.type.HarmCategory
-import com.google.ai.client.generativeai.type.SafetySetting
 import dev.eknath.aiconvo.ui.theme.AIConvoTheme
 
 class MainActivity : ComponentActivity() {
@@ -235,9 +205,6 @@ internal fun ConversationScreen(
 
 @Composable
 fun ConversationUI(input: Conv) {
-    val cardShape =
-        RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp, bottomStart = 15.dp, bottomEnd = 15.dp)
-
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = if (input.owner == Owner.AI) Arrangement.Start else Arrangement.End
@@ -255,23 +222,8 @@ fun ConversationUI(input: Conv) {
         } else if (input.state is SummarizeUiState.Loading) {
             Loading(true)
         } else if (input.state is SummarizeUiState.Success) {
-            Card(
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(5.dp)
-                    .padding(top = 5.dp),
-                colors =
-                CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-                shape = if (input.owner == Owner.USER) cardShape.copy(bottomEnd = CornerSize(0.dp)) else cardShape.copy(
-                    topStart = CornerSize(0.dp)
-                )
-            ) {
-                Text(
-                    text = input.value, modifier = Modifier
-                        .defaultMinSize(minWidth = 50.dp)
-                        .padding(5.dp)
-                )
-            }
+
+            ConversationBubble(message = input.value, sender = (input.owner == Owner.USER))
         }
     }
 }
@@ -302,6 +254,32 @@ fun NetworkErrorDialog() {
     }
 }
 
+@Composable
+fun ConversationBubble(
+    message: String,
+    sender: Boolean
+) {
+    val cardShape =
+        RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp, bottomStart = 15.dp, bottomEnd = 15.dp)
+    Card(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(5.dp)
+            .padding(top = 5.dp),
+        colors =
+        CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+        shape = if (sender) cardShape.copy(bottomEnd = CornerSize(0.dp)) else cardShape.copy(
+            topStart = CornerSize(0.dp)
+        )
+    ) {
+        Text(
+            text = message,
+            modifier = Modifier
+                .defaultMinSize(minWidth = 50.dp)
+                .padding(5.dp)
+        )
+    }
+}
 
 
 
