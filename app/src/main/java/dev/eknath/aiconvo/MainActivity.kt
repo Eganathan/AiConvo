0a126495a826af07e0129e2b47635aba8218d703
eganathan.r <eganathan.r@zohocorp.com>
2023-12-17 01:33:30 +0530

    chore: icon updated and creadit added

--- app/src/main/java/dev/eknath/aiconvo/MainActivity.kt
+++ app/src/main/java/dev/eknath/aiconvo/MainActivity.kt
@@ -1,31 +1,52 @@
 package dev.eknath.aiconvo
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.google.ai.client.generativeai.GenerativeModel
 import dev.eknath.aiconvo.ui.presentation.screens.ChatScreen
 import dev.eknath.aiconvo.ui.theme.AIConvoTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -38,46 +59,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    val generativeModel = GenerativeModel(modelName = AI_MODELS.GEMINI_PRO.key, apiKey = BuildConfig.apiKey)
+                    val generativeModel = GenerativeModel(
+                        modelName = AI_MODELS.GEMINI_PRO.key,
+                        apiKey = BuildConfig.apiKey
+                    )
                     val viewModel = ConvoViewModel(generativeModel)
 
-                    Scaffold(
-                        modifier = Modifier.padding(),
-                        topBar = {
-                            CenterAlignedTopAppBar(
-                                title = {
-                                    Row {
-                                        Text(
-                                            text = "AIConvo_",
-                                            fontWeight = FontWeight.Bold,
-                                            style = MaterialTheme.typography.headlineMedium
-                                        )
-                                    }
-                                },
-                                navigationIcon = {
-                                    IconButton(onClick = { /*TODO*/ }) {
-                                        Icon(
-                                            imageVector = Icons.Filled.Menu,
-                                            contentDescription = ""
-                                        )
-                                    }
-                                },
-                                actions = {
-                                    IconButton(onClick = { }) {
-                                        Icon(
-                                            imageVector = Icons.Filled.Settings,
-                                            contentDescription = ""
-                                        )
-                                    }
-                                },
-                                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                                    containerColor = MaterialTheme.colorScheme.primaryContainer
-                                )
-                            )
-                        }
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+
+
+                    ModalNavigationDrawer(
+                        drawerState = drawerState,
+                        drawerContent = {
+                            ModalDrawerSheet(
+                                modifier = Modifier.fillMaxWidth(0.5f),
+                                content = { DrawerContent() })
+                        },
                     ) {
-                        Box(modifier = Modifier.padding(it)) {
-                            ChatScreen(viewModel)
+                        Scaffold(
+                            modifier = Modifier.padding(),
+                            topBar = {
+                                CenterAlignedTopAppBar(
+                                    title = {
+                                        Row {
+                                            Text(
+                                                text = "AIConvo_",
+                                                fontWeight = FontWeight.Bold,
+                                                style = MaterialTheme.typography.headlineMedium
+                                            )
+                                        }
+                                    },
+                                    navigationIcon = {
+                                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                            Icon(
+                                                imageVector = Icons.Filled.Menu,
+                                                contentDescription = ""
+                                            )
+                                        }
+                                    },
+                                    actions = {
+                                        IconButton(onClick = { }) {
+                                            Icon(
+                                                imageVector = Icons.Filled.Settings,
+                                                contentDescription = ""
+                                            )
+                                        }
+                                    },
+                                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                                    )
+                                )
+                            }
+                        ) {
+                            Box(modifier = Modifier.padding(it)) {
+                                ChatScreen(viewModel)
+                            }
                         }
                     }
                 }
@@ -95,6 +132,66 @@ fun SettingSheetComponent(viewmodel: ConvoViewModel) {
     Column {
 
     }
+}
+
+
+@Composable
+fun DrawerContent() {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Column {
+            Text(text = "AIConvo")
+            Divider()
+        }
+
+        CreditComponent()
+    }
+}
+
+@Composable
+fun CreditComponent() {
+    val context = LocalContext.current
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(bottom = 10.dp)
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceAround) {
+            Text(
+                text = "Made with ",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier
+            )
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "",
+                tint = Color.Red,
+                modifier = Modifier.size(15.dp)
+            )
+        }
+        Text(
+            text = "Asmakam-AppStudio",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = "eknath.dev",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.fillMaxWidth().clickable {openUrl(context,"https:eknath.dev") }
+        )
+    }
+}
+
+fun openUrl(context: Context, url: String) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    context.startActivity(intent)
 }
 
 
