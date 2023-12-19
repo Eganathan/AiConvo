b7120f5a6db0dcd4ce21f80018d42249e2388d13
eganathan.r <eganathan.r@zohocorp.com>
2023-12-19 19:12:32 +0530

    chore: exam data wip

---
+++ app/src/main/java/dev/eknath/aiconvo/ui/data/models/ExamOptionsData.kt
@@ -0,0 +1,22 @@
+package dev.eknath.aiconvo.ui.data.models
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Exam(
+    val exam: List<Question>
+)
+
+@JsonClass(generateAdapter = true)
+data class Question(
+    val question: String,
+    val options: List<Option>,
+    val answer: Int,
+    val explanation: String
+)
+
+@JsonClass(generateAdapter = true)
+data class Option(
+    val option_id: Int,
+    val option: String
+)
